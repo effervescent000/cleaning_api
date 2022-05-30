b94b44773f8c6f969a1ee75689c75a50310f9799
effervescent000 <kislio01@gmail.com>
2022-05-30 19:29:46 -0400

    room post endpoint

--- cleaning_api/rooms.py
+++ cleaning_api/rooms.py
@@ -5,13 +5,28 @@ from .models import Room
 from .schema import RoomSchema
 from . import db, jwt
 
-bp = Blueprint("room", __name__, url_prefix="/room")
+bp = Blueprint("rooms", __name__, url_prefix="/rooms")
 one_room_schema = RoomSchema()
 multi_room_schema = RoomSchema(many=True)
 
 # GET endpoints
 
 # POST endpoints
+@bp.route("/", methods=["POST"])
+@jwt_required()
+def add_room():
+    data = request.get_json()
+    type = data.get("type")
+    if not type:
+        return jsonify({"error": "no room type specified"}), 400
+
+    label = data.get("label", type)
+
+    room = Room(type=type, label=label, user_id=current_user.id)
+    db.session.add(room)
+    db.session.commit()
+    return jsonify(one_room_schema.dump(room)), 201
+
 
 # PUT endpoints
 
