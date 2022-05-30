4630b603fa6c0b9dc63920c31e2c88b700761393
effervescent000 <kislio01@gmail.com>
2022-05-30 14:28:57 -0400

    some record factories for testing

---
+++ tests/shapes.py
@@ -0,0 +1,46 @@
+from typing import Union
+from random import randint
+from cleaning_api.models import User, Task, Room, Schedule
+
+factory_dict = dict[str, Union[str, int]]
+
+
+def user_record_factory(
+    id: int, *, username: str = None, role: str = None
+) -> factory_dict:
+    return {"id": id, "username": username or f"abcd-{id}", "role": role}
+
+
+def task_record_factory(
+    id: int, *, label: str = None, room_id: int, user_id: int
+) -> factory_dict:
+    return {
+        "id": id,
+        "label": label or randint(1, 100000),
+        "room_id": room_id,
+        "user_id": user_id,
+    }
+
+
+def room_record_factory(id: int, *, label: str = None, user_id: int) -> factory_dict:
+    return {
+        "id": id,
+        "label": label or randint(1, 100000),
+        "user_id": user_id,
+    }
+
+
+def schedule_record_factory(
+    id: int, *, points: dict = {}, user_id: int
+) -> factory_dict:
+    return {
+        "id": id,
+        "mon_points": points.get("mon", 0),
+        "tue_points": points.get("tue", 0),
+        "wed_points": points.get("wed", 0),
+        "thu_points": points.get("thu", 0),
+        "fri_points": points.get("fri", 0),
+        "sat_points": points.get("sat", 0),
+        "sun_points": points.get("sun", 0),
+        "user_id": user_id,
+    }
